ee8674bb0da1755b50d95b382623ea1046e889f6
Udalcov Artem <artemudalcov05@gmail.com>
2025-07-22 20:15:04 +0300

    added get method

--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"fmt"
 )
 
+type Courses struct{
+	ID string `json:"id"`
+	Title string `json:"name"`
+}
 
+var courses = []Courses{
+	{"100","Grokking Modern System Design "},
+	{"101","CloudLab: WebSockets-based Chat Application using API Gateway"},
+}
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from SigmaWater")
+	jsonData, err := json.Marshal(courses)
+	if err != nil{
+		fmt.Fprint(w, err.Error())
+		return
+	}
+	fmt.Fprint(w, string(jsonData))
+	// w.Header().Set("Content-Type", "application/json")
+	// json.NewEncoder(w).Encode(courses)
 }
 
 func main() {
-	http.HandleFunc("/", getHandler)
+	http.HandleFunc("/courses", getHandler)
 	http.ListenAndServe(":8080", nil)
 }
