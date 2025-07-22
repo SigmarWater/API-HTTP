130461aec4453b00075e49aa8ce0c725e2d9615d
Udalcov Artem <artemudalcov05@gmail.com>
2025-07-22 21:12:54 +0300

    finished rest-api

--- main.go
+++ main.go
@@ -5,15 +5,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Courses struct{
+type Course struct{
 	ID string `json:"id"`
 	Title string `json:"name"`
 }
 
-var courses = []Courses{
+var courses = []Course{
 	{"100","Grokking Modern System Design "},
 	{"101","CloudLab: WebSockets-based Chat Application using API Gateway"},
 }
+
+func updateCourse(c *gin.Context){
+	courseIdtoUpdate := c.Param("id")
+	var updatedCourse Course
+	c.BindJSON(&updatedCourse)
+
+	for index, course := range courses {
+		if course.ID == courseIdtoUpdate {
+			courses[index] = updatedCourse
+			c.IndentedJSON(http.StatusOK, courses)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "course not found!"})
+}
+
 
 func getCourses(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, courses)
@@ -31,7 +47,7 @@ func getSpecificCourse(c *gin.Context){
 }
 
 func addCourse(c *gin.Context){
-	var courseToAdd Courses
+	var courseToAdd Course
 	err := c.ShouldBindJSON(&courseToAdd)
 	if err != nil{
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err})
@@ -41,11 +57,25 @@ func addCourse(c *gin.Context){
 	c.IndentedJSON(http.StatusCreated, courses)
 }
 
+func deleteCourse(c *gin.Context){
+	var id = c.Param("id")
+	for index, course := range courses{
+		if course.ID == id{
+			courses = append(courses[:index], courses[index + 1:]...)
+			c.IndentedJSON(http.StatusOK, courses)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "course not found!"})
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/courses", getCourses)
 	r.GET("/courses/:id", getSpecificCourse)
 	r.POST("/courses", addCourse)
+	r.PUT("/course/:id", updateCourse)
+	r.DELETE("course/:id", deleteCourse)
 
 	r.Run(":8080")
 }
